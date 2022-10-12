72df3d9a68c9d29fa8ce97abb1fcf3b8974bba3b
FanchinAlberto <6190192@itisrossi.vi.it>
2022-10-13 00:09:44 +0200

    modifiche finali

--- DandaDo/public/dist/script.js
+++ DandaDo/public/dist/script.js
@@ -1,6 +1,9 @@
 //manipolazione dati api
 assignToDo();
 
+const counterToDo = 4;
+const counterDoing = 4;
+
 function fetchRandom(pid) {
 let currentP = document.getElementById(pid);
 fetch('https://dummyjson.com/todos/random')
@@ -63,3 +66,132 @@ window.addEventListener("load", function () {
     });
   });
 });
+
+function showDiv1(){  
+    $("#aggiuntaNewActivity").fadeToggle("slow");
+}
+
+  function showDiv2(){    
+      $("#aggiuntaOngoingActivity").fadeToggle("slow");
+  }
+
+function addToDo(){
+   $("#ToDoAddButton").before(`<div class="flex bg-black rounded-2xl text-white py-3 text-lg justify-between ma" id="done${counterToDo}">
+   <p id="doneText${counterToDo}">${document.getElementById("inputModulo1").value}</p>
+   <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>         
+   
+     <div x-data="{ dropdownOpen: false }" class="relative">
+       <button @click="dropdownOpen = !dropdownOpen" class="relative z-10 block bg-gray-800 rounded p-2 hover:bg-gray-700 focus:outline-none focus:bg-gray-700">
+         <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+         </svg>
+       </button>
+   
+       <div x-show="dropdownOpen" @click="dropdownOpen = false" class="fixed inset-0 h-full w-full z-10"></div>
+   
+       <div x-show="dropdownOpen" class="absolute right-0 mt-2 w-48 bg-white rounded-md overflow-hidden shadow-xl z-20 text-black">
+         <a onclick="moveToDiv('done${counterToDo}', 'done', 'DoneAddButton')" class="block px-4 py-2 text-sm text-gray-800 border-b hover:bg-gray-200"><span class="text-gray-600">Mark as: Done</span></a>
+         <a onclick="moveToDiv('done${counterToDo}', 'doing', 'DoingAddButton')" class="block px-4 py-2 text-sm text-gray-800 border-b hover:bg-gray-200"><span class="text-gray-600">Mark as: Doing</span></a>
+         <a onclick="removeDiv('done${counterToDo}')" class="block px-4 py-2 text-sm text-gray-800 border-b hover:bg-gray-200"><span class="text-gray-600">Remove</span></a>
+       </div>
+     </div> 
+  </div>
+  `);  
+  }
+  function addDoing(){
+
+    $("#DoingAddButton").before(`<div class="flex bg-black rounded-2xl text-white py-3 text-lg justify-between ma" id="done${counterDoing}">
+    <p id="doneText${counterDoing}">${document.getElementById("inputModulo2").value}</p>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>         
+    
+      <div x-data="{ dropdownOpen: false }" class="relative">
+        <button @click="dropdownOpen = !dropdownOpen" class="relative z-10 block bg-gray-800 rounded p-2 hover:bg-gray-700 focus:outline-none focus:bg-gray-700">
+          <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+          </svg>
+        </button>
+    
+        <div x-show="dropdownOpen" @click="dropdownOpen = false" class="fixed inset-0 h-full w-full z-10"></div>
+    
+        <div x-show="dropdownOpen" class="absolute right-0 mt-2 w-48 bg-white rounded-md overflow-hidden shadow-xl z-20 text-black">
+          <a onclick="moveToDiv('done${counterDoing}', 'done', 'DoneAddButton')" class="block px-4 py-2 text-sm text-gray-800 border-b hover:bg-gray-200"><span class="text-gray-600">Mark as: Done</span></a>
+          <a onclick="moveToDiv('done${counterDoing}', 'doing', 'DoingAddButton')" class="block px-4 py-2 text-sm text-gray-800 border-b hover:bg-gray-200"><span class="text-gray-600">Mark as: Doing</span></a>
+          <a onclick="removeDiv('done${counterDoing}')" class="block px-4 py-2 text-sm text-gray-800 border-b hover:bg-gray-200"><span class="text-gray-600">Remove</span></a>
+        </div>
+      </div> 
+   </div>
+   `);
+   }
+
+   $(document).ready(function () {
+    $('#panel').DataTable();
+  });
+  
+  $(document).ready(function(){
+  $("#TitTable").click(function(){
+  $("#panel").fadeToggle("slow");
+  
+  });
+  });
+  
+  
+  $(document).ready(function(){
+  $("#TitTable").click(function(){
+  $("#panel_filter").fadeToggle("slow");
+  
+  });
+  });
+  
+  $(document).ready(function(){
+  $("#panel_paginate").remove();
+  });
+  
+  $(document).ready(function(){
+  $("#panel_info").remove();
+  });
+  
+  $(document).ready(function(){
+  $("#panel_length").remove();
+  });
+  
+  var time= 0;
+  var tempo = 0;
+  const count = document.getElementById("timer");
+  
+  function setTimerSetup(){
+    time = document.getElementById("inputTimer").value;
+    tempo = time * 60;
+  }
+
+  function startTimer(){
+    setInterval(aggiornatimer,1000)
+    $("#timerContainer").fadeToggle("slow");
+  }
+
+  function aggiornatimer(){
+    
+    
+    if(startTimer<60){
+        let minuti = Math.floor(tempo/60)
+        let secondi = tempo % 60
+        secondi = secondi < 10 ? '0' + secondi : secondi
+        count.innerHTML = `${minuti}:${secondi}`
+    }
+    else{
+        let ore = Math.floor((tempo/60)/60)
+        let minuti = Math.floor(tempo/60)%60
+        let secondi = tempo % 60
+        secondi = secondi < 10 ? '0' + secondi : secondi
+        minuti = minuti < 10 ? '0' + minuti : minuti
+        count.innerHTML = `${ore}:${minuti}:${secondi}`
+    }
+     
+    tempo--
+}
+
+function showTimerSetup(){
+  $("#aggiuntaTimer").fadeToggle("slow");
+}
+
+
+
